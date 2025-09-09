44667490207a10d1128e7300b38e56c7ec891ac4
gyannbaybayoncordero-eng <gyannbaybayon.cordero@gmail.com>
2025-09-09 11:01:37 +0000

    Seatwork 11 part 1 and 2

--- Seatwork 11 part 2.java
+++ Seatwork 11 part 2.java
@@ -1,1 +1,11 @@
+public class Seatwork 11 part 2 {
+public static void main(String[] args) {
+         
+System.out.println("SEATWORKELEVENPART2");
+System.out.println();
+int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+for (int row = 0; row < myNumbers.length; row++) {
+for (int col = 0; col < myNumbers[row].length; col++) {
+System.out.println("myNumbers[" + row + "][" + col + "] = " + myNumbers[row][col])                                                                                       }
 
